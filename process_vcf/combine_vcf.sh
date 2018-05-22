16a445a7b01a906b91f821cb71a6df189157c7ab
Xin Li <usigwt@gmail.com>
2018-05-22 11:04:40 -0700

    Tue May 22 11:04:40 PDT 2018

---
+++ process_vcf/combine_vcf.sh
@@ -0,0 +1,18 @@
+
+
+# ls /srv/scratch/restricted/rare_diseases/data/vcfs/for_freeze/*_homogenized_short.vcf.gz | xargs vcf-merge --ref-for-missing 0/0 | bgzip -c >
+
+# GTEx VEP example
+
+##VEP=v83 cache=/humgen/atgu1/fs03/konradk/vep//homo_sapiens/83_GRCh37 db=. polyphen=2.2.2 sift=sift5.2.2 COSMIC=71 ESP=20141103 gencode=GENCODE 19 HGMD-PUBLIC=20152 genebuild=2011-04 regbuild=13 assembly=GRCh37.p13 dbSNP=144 ClinVar=201507
+##LoF=Loss-of-function annotation (HC = High Confidence; LC = Low Confidence)
+##LoF_filter=Reason for LoF not being HC
+##LoF_flags=Possible warning flags for LoF
+##LoF_info=Info used for LoF annotation
+##INFO=<ID=CSQ,Number=.,Type=String,Description="Consequence annotations from Ensembl VEP. Format: Allele|Consequence|IMPACT|SYMBOL|Gene|Feature_type|Feature|BIOTYPE|EXON|INTRON|HG
+
+
+
+
+
+
