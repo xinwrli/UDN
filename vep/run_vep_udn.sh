863e1553f5bd02d02e93f2a860ec8e55094b09a6
Xin Li <usigwt@gmail.com>
2018-05-25 16:12:16 -0700

    Fri May 25 16:12:16 PDT 2018

---
+++ vep/run_vep_udn.sh
@@ -0,0 +1,11 @@
+
+
+
+
+nohup /users/xli6/data/xin/tools/ensembl-vep/vep --species homo_sapiens --assembly GRCh37 --input_file /users/xli6/data/xin/udn/vcf/all_homogenized_short.vcf.gz --format vcf --output_file STDOUT --cache --dir_cache /users/xli6/data/xin/tools/ensembl-vep/vep_cache --fasta /users/xli6/data/xin/tools/ensembl-vep/vep_cache/homo_sapiens/91_GRCh37/Homo_sapiens.GRCh37.75.dna.primary_assembly.fa.gz --cache_version 91 --everything --allele_number --vcf --offline --force_overwrite
+
+
+
+
+
+
